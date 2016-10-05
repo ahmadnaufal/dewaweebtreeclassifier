a19a9d390061c0795ffd682dd3094930a0dc2a19
Ahmad N. F <fahmadnaufal@gmail.com>
2016-10-05 17:21:19 +0700

    ID3 tested and working

--- src/dewaweebtreeclassifier/DewaWeebTreeClassifier.java
+++ src/dewaweebtreeclassifier/DewaWeebTreeClassifier.java
@@ -395,7 +395,7 @@ public class DewaWeebTreeClassifier {
                         break;
                 }
             } catch (Exception ex) {
-                System.out.println(ex.toString());
+                ex.printStackTrace();
                 break;  
             }
         }
